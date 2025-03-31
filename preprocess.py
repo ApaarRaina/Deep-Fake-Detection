ec823ca39fcaf533ceef89c55e45c146ecc45d21
ApaarRaina <apaarraina@gmail.com>
2025-03-31 17:07:18 +0530

    updated

--- preprocess.py
+++ preprocess.py
@@ -2,14 +2,14 @@ import pandas as pd
 import cv2 as cv
 import os
 
-df=pd.read_csv('valid.csv')
+df=pd.read_csv('test.csv')
 
 df.drop(columns=['Unnamed: 0','original_path','id'],inplace=True)
 
 
 clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 
-output_dir = 'C:/programs/Deep_Fake_detection/processed_valid'
+output_dir = 'C:/programs/Deep_Fake_detection/processed_test'
 
 # Create the directory if it does not exist
 if not os.path.exists(output_dir):
@@ -24,7 +24,7 @@ for i in range(len(df)):
     img=clahe.apply(img) #brightness adjust
 
     # Construct new path: Replace "train/" with "processed_train/"
-    relative_path = os.path.relpath(path, start='valid')  # e.g., 'real/31355.jpg'
+    relative_path = os.path.relpath(path, start='test')  # e.g., 'real/31355.jpg'
     new_path = os.path.join(output_dir, relative_path)  # e.g., 'processed_train/real/31355.jpg'
 
     # Ensure the subdirectory exists
